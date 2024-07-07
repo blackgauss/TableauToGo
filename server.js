779cb6b9c1f93ce2b6230926504f7083673a169b
blackgauss <81921810+blackgauss@users.noreply.github.com>
2024-07-07 14:09:49 -0700

    troubleshooting vercel

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Set the view engine to ejs
 app.set('view engine', 'ejs');
+
+// Serve static files from the "public" directory
 app.use(express.static('public'));
 
+// Define the route for the home page
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
